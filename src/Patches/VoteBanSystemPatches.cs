72af5e84838dc09e8a52bdfe29fe83ba178346f5
kfctoparbuz-del <kfctoparbuz@gmail.com>
2026-05-04 10:00:35 +0300

    Update VoteBanSystemPatches.cs

--- src/Patches/VoteBanSystemPatches.cs
+++ src/Patches/VoteBanSystemPatches.cs
@@ -5,7 +5,7 @@ namespace MalumMenu;
 [HarmonyPatch(typeof(VoteBanSystem), nameof(VoteBanSystem.AddVote))]
 public static class VoteBanSystem_AddVote
 {
-    // Prefix patch of VoteBanSystem.AddVote to instantly kick players when host votes to kick them
+    // Префикс-патч VoteBanSystem.AddVote для мгновенного кика игрока, когда хост голосует за его кик
     public static bool Prefix(VoteBanSystem __instance, int srcClient, int clientId)
     {
         if (!Utils.isHost) return true;
@@ -22,7 +22,7 @@ public static class VoteBanSystem_AddVote
 [HarmonyPatch(typeof(VoteBanSystem), nameof(VoteBanSystem.CmdAddVote))]
 public static class VoteBanSystem_CmdAddVote
 {
-    // Prefix patch of VoteBanSystem.CmdAddVote to prevent AddVoteBan RPC from being sent when host votes to kick a player
+    // Префикс-патч VoteBanSystem.CmdAddVote для предотвращения отправки RPC AddVoteBan, когда хост голосует за кик игрока
     public static bool Prefix()
     {
         return !Utils.isHost;
