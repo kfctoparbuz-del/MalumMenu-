26a41f61a4047d4b8991c4fb7a24b2d09d1231ec
kfctoparbuz-del <kfctoparbuz@gmail.com>
2026-05-04 09:50:03 +0300

    Translate ShipTab UI elements to Russian
    Updated UI text in ShipTab to Russian.

--- src/UI/Windows/Tabs/ShipTab.cs
+++ src/UI/Windows/Tabs/ShipTab.cs
@@ -4,7 +4,7 @@ namespace MalumMenu;
 
 public class ShipTab : ITab
 {
-    public string name => "Ship";
+    public string name => "Корабль";
 
     public void Draw()
     {
@@ -31,46 +31,46 @@ public class ShipTab : ITab
 
     private void DrawGeneral()
     {
-        CheatToggles.unfixableLights = GUILayout.Toggle(CheatToggles.unfixableLights, " Unfixable Lights");
+        CheatToggles.unfixableLights = GUILayout.Toggle(CheatToggles.unfixableLights, " Неисправимый свет");
 
-        // CheatToggles.reportBody = GUILayout.Toggle(CheatToggles.reportBody, " Report Body");
+        // CheatToggles.reportBody = GUILayout.Toggle(CheatToggles.reportBody, " Сообщить о теле");
 
-        CheatToggles.callMeeting = GUILayout.Toggle(CheatToggles.callMeeting, " Call Meeting");
+        CheatToggles.callMeeting = GUILayout.Toggle(CheatToggles.callMeeting, " Созвать собрание");
 
-        CheatToggles.closeMeeting = GUILayout.Toggle(CheatToggles.closeMeeting, " Close Meeting");
+        CheatToggles.closeMeeting = GUILayout.Toggle(CheatToggles.closeMeeting, " Закрыть собрание");
 
-        CheatToggles.autoOpenDoorsOnUse = GUILayout.Toggle(CheatToggles.autoOpenDoorsOnUse, " Auto-Open Doors On Use");
+        CheatToggles.autoOpenDoorsOnUse = GUILayout.Toggle(CheatToggles.autoOpenDoorsOnUse, " Авто-открытие дверей при использовании");
     }
 
     private void DrawSabotage()
     {
-        GUILayout.Label("Sabotage", GUIStylePreset.TabSubtitle);
+        GUILayout.Label("Саботаж", GUIStylePreset.TabSubtitle);
 
-        CheatToggles.reactorSab = GUILayout.Toggle(CheatToggles.reactorSab, " Reactor");
+        CheatToggles.reactorSab = GUILayout.Toggle(CheatToggles.reactorSab, " Реактор");
 
-        CheatToggles.oxygenSab = GUILayout.Toggle(CheatToggles.oxygenSab, " Oxygen");
+        CheatToggles.oxygenSab = GUILayout.Toggle(CheatToggles.oxygenSab, " Кислород");
 
-        CheatToggles.elecSab = GUILayout.Toggle(CheatToggles.elecSab, " Lights");
+        CheatToggles.elecSab = GUILayout.Toggle(CheatToggles.elecSab, " Свет");
 
-        CheatToggles.commsSab = GUILayout.Toggle(CheatToggles.commsSab, " Comms");
+        CheatToggles.commsSab = GUILayout.Toggle(CheatToggles.commsSab, " Связь");
 
-        CheatToggles.showDoorsMenu = GUILayout.Toggle(CheatToggles.showDoorsMenu, " Show Doors Menu");
+        CheatToggles.showDoorsMenu = GUILayout.Toggle(CheatToggles.showDoorsMenu, " Показать меню дверей");
 
-        CheatToggles.mushSab = GUILayout.Toggle(CheatToggles.mushSab, " Mushroom Mixup");
+        CheatToggles.mushSab = GUILayout.Toggle(CheatToggles.mushSab, " Грибная неразбериха");
 
-        CheatToggles.mushSpore = GUILayout.Toggle(CheatToggles.mushSpore, " Trigger Spores");
+        CheatToggles.mushSpore = GUILayout.Toggle(CheatToggles.mushSpore, " Активировать споры");
 
-        CheatToggles.sabotageMap = GUILayout.Toggle(CheatToggles.sabotageMap, " Open Sabotage Map");
+        CheatToggles.sabotageMap = GUILayout.Toggle(CheatToggles.sabotageMap, " Открыть карту саботажа");
     }
 
     private void DrawVents()
     {
-        GUILayout.Label("Vents", GUIStylePreset.TabSubtitle);
+        GUILayout.Label("Вентиляция", GUIStylePreset.TabSubtitle);
 
-        CheatToggles.unlockVents = GUILayout.Toggle(CheatToggles.unlockVents, " Unlock Vents");
+        CheatToggles.unlockVents = GUILayout.Toggle(CheatToggles.unlockVents, " Разблокировать вентиляцию");
 
-        CheatToggles.kickVents = GUILayout.Toggle(CheatToggles.kickVents, " Kick All From Vents");
+        CheatToggles.kickVents = GUILayout.Toggle(CheatToggles.kickVents, " Выгнать всех из вентиляции");
 
-        CheatToggles.walkInVents = GUILayout.Toggle(CheatToggles.walkInVents, " Walk In Vents");
+        CheatToggles.walkInVents = GUILayout.Toggle(CheatToggles.walkInVents, " Ходить в вентиляции");
     }
 }
