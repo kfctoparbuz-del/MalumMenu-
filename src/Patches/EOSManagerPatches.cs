96a8644c8f431df6efeacc840bfb8c813c29d60e
kfctoparbuz-del <kfctoparbuz@gmail.com>
2026-05-04 10:15:41 +0300

    Update comments and patches in EOSManagerPatches

--- src/Patches/EOSManagerPatches.cs
+++ src/Patches/EOSManagerPatches.cs
@@ -3,23 +3,23 @@ using HarmonyLib;
 
 namespace MalumMenu;
 
-// GuestMode cheats are commented out as they are broken in latest updates
+// Читы GuestMode закомментированы, так как они сломаны в последних обновлениях
 
 // [HarmonyPatch(typeof(EOSManager), nameof(EOSManager.StartInitialLoginFlow))]
 // public static class EOSManager_StartInitialLoginFlow
 // {
 //     /// <summary>
-//     /// Prefix patch of EOSManager.StartInitialLoginFlow to automatically play with a guest account
-//     /// when loading the game with guestMode enabled
+//     /// Префикс-патч EOSManager.StartInitialLoginFlow для автоматической игры с гостевым аккаунтом
+//     /// при загрузке игры с включённым guestMode
 //     /// </summary>
-//     /// <param name="__instance">The <c>EOSManager</c> instance.</param>
-//     /// <returns><c>false</c> to skip the original method, <c>true</c> to allow the original method to run.</returns>
+//     /// <param name="__instance">Экземпляр <c>EOSManager</c>.</param>
+//     /// <returns><c>false</c> для пропуска оригинального метода, <c>true</c> для выполнения оригинального метода.</returns>
 //     public static bool Prefix(EOSManager __instance)
 //     {
-//         // Always delete old guest accounts to avoid merge account popup
+//         // Всегда удалять старые гостевые аккаунты, чтобы избежать всплывающего окна слияния аккаунтов
 //         __instance.DeleteDeviceID(new System.Action(__instance.EndMergeGuestAccountFlow));
 
-//         // Log into a new temp account if the user is playing in guest mode
+//         // Войти в новую временную учётную запись, если пользователь играет в гостевом режиме
 //         if (!MalumMenu.guestMode.Value) return true;
 //         __instance.StartTempAccountFlow();
 //         __instance.CloseStartupWaitScreen();
@@ -31,7 +31,7 @@ namespace MalumMenu;
 [HarmonyPatch(typeof(EOSManager), nameof(EOSManager.HasServerTimestamp), MethodType.Getter)]
 public static class EOSManager_HasServerTimestamp_Getter
 {
-    // Postfix patch of EOSManager.HasServerTimestamp Getter method to ensure the date can be spoofed
+    // Постфикс-патч геттера EOSManager.HasServerTimestamp для обеспечения возможности подмены даты
     public static void Postfix(ref bool __result)
     {
         if (!CheatToggles.spoofAprilFoolsDate) return;
@@ -43,7 +43,7 @@ public static class EOSManager_HasServerTimestamp_Getter
 [HarmonyPatch(typeof(EOSManager), nameof(EOSManager.ApproximateServerTime), MethodType.Getter)]
 public static class EOSManager_ApproximateServerTime_Getter
 {
-    // Postfix patch of EOSManager.ApproximateServerTime Getter method to spoof the date to April 1st, 7:01 AM UTC
+    // Постфикс-патч геттера EOSManager.ApproximateServerTime для подмены даты на 1 апреля, 7:01 UTC
     public static void Postfix(ref Il2CppSystem.DateTime __result)
     {
         if (!CheatToggles.spoofAprilFoolsDate) return;
@@ -56,7 +56,7 @@ public static class EOSManager_ApproximateServerTime_Getter
 [HarmonyPatch(typeof(EOSManager), nameof(EOSManager.IsFreechatAllowed))]
 public static class EOSManager_IsFreechatAllowed
 {
-    // Prefix patch of EOSManager.IsFreechatAllowed to unlock freechat
+    // Префикс-патч EOSManager.IsFreechatAllowed для разблокировки свободного чата
     public static void Postfix(ref bool __result)
     {
         if (CheatToggles.unlockFeatures)
@@ -69,7 +69,7 @@ public static class EOSManager_IsFreechatAllowed
 [HarmonyPatch(typeof(EOSManager), nameof(EOSManager.IsFriendsListAllowed))]
 public static class EOSManager_IsFriendsListAllowed
 {
-    // Prefix patch of EOSManager.IsFriendsListAllowed to unlock friend list
+    // Префикс-патч EOSManager.IsFriendsListAllowed для разблокировки списка друзей
     public static void Postfix(ref bool __result)
     {
         if (CheatToggles.unlockFeatures)
@@ -82,7 +82,7 @@ public static class EOSManager_IsFriendsListAllowed
 [HarmonyPatch(typeof(EOSManager), nameof(EOSManager.IsAllowedOnline))]
 public static class EOSManager_IsAllowedOnline
 {
-    // Prefix patch of EOSManager.IsAllowedOnline to allow online games
+    // Префикс-патч EOSManager.IsAllowedOnline для разрешения онлайн-игр
     public static void Prefix(ref bool canOnline)
     {
         if (CheatToggles.unlockFeatures)
@@ -95,7 +95,7 @@ public static class EOSManager_IsAllowedOnline
 [HarmonyPatch(typeof(EOSManager), nameof(EOSManager.IsMinorOrWaiting))]
 public static class EOSManager_IsMinorOrWaiting
 {
-    // Prefix patch of EOSManager.IsMinorOrWaiting to remove minor status
+    // Префикс-патч EOSManager.IsMinorOrWaiting для снятия статуса несовершеннолетнего
     public static void Postfix(ref bool __result)
     {
         if (CheatToggles.unlockFeatures)
